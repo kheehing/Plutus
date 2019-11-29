33fa1f7057fa953783c7277e36baa52c43514c09
kheehing <lightkheehing@gmail.com>
2019-11-29 12:00:46 +0800

    running

---
+++ Plutus/Main/import.h
@@ -0,0 +1,10 @@
+//
+//  import.h
+//  Plutus
+//
+//  Created by ITP312 on 29/11/19.
+//  Copyright © 2019 NYP. All rights reserved.
+//
+
+#import "FRHyperLabel"
+
---
+++ Pods/Target Support Files/Pods-Plutus-PlutusUITests/Pods-Plutus-PlutusUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Plutus_PlutusUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Plutus_PlutusUITests
+@end
